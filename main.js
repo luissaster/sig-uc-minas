b97ec12712692bae527058a8fffcf0a773eeeaf1
Luís Fernando Almeida <luisfernalme@gmail.com>
2025-06-15 20:04:34 -0300

    layer sidebar

---
+++ main.js
@@ -0,0 +1,140 @@
+// 1. Inicializa o mapa e define a visão inicial (latitude, longitude, zoom)
+var map = L.map("map").setView([-14.235, -51.925], 5); // Centro do Brasil
+
+// 2. Camada base do OpenStreetMap
+var osm = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+}).addTo(map);
+
+// 3. Camadas WMS do GeoServer
+var geoserverUrl = "http://localhost:8080/geoserver/wms";
+var ucEstaduais = L.tileLayer.wms(geoserverUrl, {
+  layers: "projeto_sin420:uc_estaduais",
+  format: "image/png",
+  transparent: true,
+  version: "1.1.0",
+});
+
+var ucMunicipais = L.tileLayer.wms(geoserverUrl, {
+  layers: "projeto_sin420:uc_municipais",
+  format: "image/png",
+  transparent: true,
+  version: "1.1.0",
+});
+
+var ucFederais = L.tileLayer.wms(geoserverUrl, {
+  layers: "projeto_sin420:uc_federais",
+  format: "image/png",
+  transparent: true,
+  version: "1.1.0",
+});
+
+var limiteMG = L.tileLayer.wms(geoserverUrl, {
+  layers: "projeto_sin420:limite_mg",
+  format: "image/png",
+  transparent: true,
+  version: "1.1.0",
+});
+
+var areaCriacaoUC = L.tileLayer.wms(geoserverUrl, {
+  layers: "projeto_sin420:area_criacao_uc",
+  format: "image/png",
+  transparent: true,
+  version: "1.1.0",
+});
+
+var biomasMG = L.tileLayer.wms(geoserverUrl, {
+  layers: "projeto_sin420:biomas_mg",
+  format: "image/png",
+  transparent: true,
+  version: "1.1.0",
+});
+
+// 4. Controle de camadas
+var baseMaps = {
+  OpenStreetMap: osm,
+};
+
+var overlayMaps = {
+  "UC Estaduais": ucEstaduais,
+  "UC Municipais": ucMunicipais,
+  "UC Federais": ucFederais,
+  "Limite de MG": limiteMG,
+  "Área de Criação de UC": areaCriacaoUC,
+  "Biomas de MG": biomasMG,
+};
+
+L.control.layers(baseMaps, overlayMaps).addTo(map);
+
+// ---------------- Sidebar ----------------
+var layerListEl = document.getElementById("layerList");
+
+var layerData = [
+  { name: "UC Estaduais", layer: ucEstaduais },
+  { name: "UC Municipais", layer: ucMunicipais },
+  { name: "UC Federais", layer: ucFederais },
+  { name: "Limite de MG", layer: limiteMG },
+  { name: "Área de Criação de UC", layer: areaCriacaoUC },
+  { name: "Biomas de MG", layer: biomasMG },
+];
+
+layerData.forEach(function (item) {
+  var li = document.createElement("li");
+  li.className = "layer-item";
+  li.setAttribute("data-name", item.name);
+
+  // Adiciona o handle de arrasto
+  var dragHandle = document.createElement("div");
+  dragHandle.className = "drag-handle";
+  
+  // Adiciona o nome da camada
+  var label = document.createElement("span");
+  label.textContent = item.name;
+  
+  // Adiciona o checkbox
+  var checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  checkbox.addEventListener("change", function () {
+    if (this.checked) {
+      item.layer.addTo(map);
+    } else {
+      map.removeLayer(item.layer);
+    }
+  });
+
+  // Adiciona os elementos na ordem correta
+  li.appendChild(dragHandle);
+  li.appendChild(label);
+  li.appendChild(checkbox);
+  
+  layerListEl.appendChild(li);
+});
+
+if (typeof Sortable !== "undefined") {
+  new Sortable(layerListEl, {
+    animation: 150,
+    handle: '.drag-handle',
+    ghostClass: 'dragging',
+    onStart: function(evt) {
+      evt.item.classList.add('dragging');
+    },
+    onEnd: function(evt) {
+      evt.item.classList.remove('dragging');
+      updateLayerOrder();
+    }
+  });
+}
+
+function updateLayerOrder() {
+  var items = Array.from(layerListEl.querySelectorAll("li")).reverse();
+  items.forEach(function (li) {
+    var name = li.getAttribute("data-name");
+    var obj = layerData.find(function (d) {
+      return d.name === name;
+    });
+    if (obj && map.hasLayer(obj.layer)) {
+      obj.layer.bringToFront();
+    }
+  });
+}
